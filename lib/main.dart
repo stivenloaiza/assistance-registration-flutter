d64b6600bf47c0cdff5401b2a234f6d3a534983e
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-12-09 08:45:12 -0500

    Add config qr and scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:asia_project/views/qr-dinamic/qr_reader_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainScreen(),
     );
   }
 }
