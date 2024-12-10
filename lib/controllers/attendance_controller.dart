6992151ba42c6fab8f5dfa789eac164e308f3bb5
joseCardona12 <josesiprozmater@gmail.com>
2024-12-10 10:24:37 -0500

    feat: Add method in the controller attendance

--- lib/controllers/attendance_controller.dart
+++ lib/controllers/attendance_controller.dart
@@ -2,7 +2,9 @@
 import 'package:asia_project/models/attendance_model.dart';
 import 'package:asia_project/ports/attendance_port.dart';
 import 'package:asia_project/services/attendance_service.dart';
+import 'package:asia_project/widgets/reports_bi_widgets/bar_chart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class AttendanceController implements AttendancePort{
   final AttendanceService _attendanceService;
@@ -22,5 +24,75 @@ class AttendanceController implements AttendancePort{
   @override
   Future<List<AttendanceModel>> findByProperty(String property, String valueProperty) async{
     return await _attendanceService.getAttendanceByProperty(property, valueProperty);
+  }
+
+  @override
+  Future<Map<String, Map<String, dynamic>>> findAttendanceByUserAndDateRange(String userId, DateTimeRange dateRange) async{
+    try {
+      final QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('attendance')
+          .where('user', isEqualTo: userId)
+          .where('timeStamp', isGreaterThanOrEqualTo: dateRange.start)
+          .where('timeStamp', isLessThanOrEqualTo: dateRange.end)
+          .get();
+
+      final Map<String, Map<String, dynamic>> groupAttendance = {};
+
+      for (var doc in snapshot.docs) {
+        final data = doc.data() as Map<String, dynamic>;
+        final String groupId = data['group'];
+        final String status = data['attendanceStatus'];
+
+        groupAttendance[groupId] ??= {'late': 0, 'onTime': 0, 'absent': 0};
+        groupAttendance[groupId]?[status] =
+            (groupAttendance[groupId]?[status] ?? 0) + 1;
+      }
+
+      return groupAttendance;
+    } catch (e) {
+      throw Exception("Error fetching attendance data: $e");
+    }
+  }
+
+  @override
+  List<ChartData> processAttendanceForWidget(Map<String, Map<String, dynamic>> groupAttendance) {
+    final List<ChartData> chartData = [];
+    groupAttendance.forEach((groupId, attendance) {
+      final int totalRecords = attendance.values.fold<int>(
+        0,
+            (sum, value) => sum + (value as int),
+      );
+      final double absentPercentage =
+          ((attendance['absent'] ?? 0) / totalRecords) * 100;
+      final double onTimePercentage =
+          ((attendance['onTime'] ?? 0) / totalRecords) * 100;
+      final double latePercentage =
+          ((attendance['late'] ?? 0) / totalRecords) * 100;
+
+      chartData.add(ChartData(
+        barTitle: groupId,
+        numberFirstValue: absentPercentage,
+        numberSecondValue: onTimePercentage,
+        numberThirdValue: latePercentage,
+      ));
+    });
+
+    return chartData;
+  }
+
+  @override
+  Future<BarChartWidget> buildAttendanceWidget(String userId, DateTimeRange dateRange, String chartTitle) async{
+    final groupAttendance = await findAttendanceByUserAndDateRange(userId, dateRange);
+    final List<ChartData> chartData = processAttendanceForWidget(groupAttendance);
+
+    return BarChartWidget(
+      chartTitle: chartTitle,
+      ref: {
+        'titleFirstValue': 'Absent',
+        'titleSecondValue': 'On-Time',
+        'titleThirdValue': 'Late',
+      },
+      data: chartData,
+    );
   }
 }
--- lib/ports/attendance_port.dart
+++ lib/ports/attendance_port.dart
@@ -1,8 +1,13 @@
 
 import 'package:asia_project/models/attendance_model.dart';
+import 'package:asia_project/widgets/reports_bi_widgets/bar_chart.dart';
+import 'package:flutter/material.dart';
 
 abstract class AttendancePort{
   Future<List<AttendanceModel>> findAllAttendance();
   Future<List<AttendanceModel>> findAttendanceByDateRange(String range);
   Future<List<AttendanceModel>> findByProperty(String Property, String valueProperty);
+  Future<Map<String, Map<String,dynamic>>> findAttendanceByUserAndDateRange(String userId, DateTimeRange dateRange);
+  List<ChartData> processAttendanceForWidget(Map<String, Map<String, dynamic>> groupAttendance);
+  Future<BarChartWidget> buildAttendanceWidget(String userId, DateTimeRange dateRange, String chartTitle);
 }
