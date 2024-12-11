9d485909fcbc4774fd00587965e18fbf768e66e0
devBitan <jquiramadev@outlook.com>
2024-12-11 09:32:57 -0500

    fix:input date register, initial route

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: '/login', // Define la ruta inicial
       routes: {
-        '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+        '/': (context) =>  LoginPage(),
         '/register': (context) => RegisterPage(),
         '/login': (context) => LoginPage(),
         '/loginDevice' : (context) => login_devices(),
@@ -64,73 +64,73 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            // Text(
-            //   '$_counter',
-            //   style: Theme.of(context).textTheme.headlineMedium,
-            // ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/register'); // Navega a la página de registro
-              },
-              child: const Text('Go to Register'),
-            ),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DeviceManagementApp(),
-              ),
-            );
-            },
-            child: const Text("Device Management")
-            ),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => NotificationsPage(),
-              ),
-            );
-            },
-            child: const Text("Vista notificaciones")
-            ),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LoginPage(),
-              ),
-            );
-            },
-            child: const Text("Login")
-            ),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => login_devices(),
-              ),
-            );
-            },
-            child: const Text("login devices")
-            )
-          ],
-        ),
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: <Widget>[
+        //     const Text(
+        //       'You have pushed the button this many times:',
+        //     ),
+        //     // Text(
+        //     //   '$_counter',
+        //     //   style: Theme.of(context).textTheme.headlineMedium,
+        //     // ),
+        //     ElevatedButton(
+        //       onPressed: () {
+        //         Navigator.pushNamed(
+        //             context, '/register'); // Navega a la página de registro
+        //       },
+        //       child: const Text('Go to Register'),
+        //     ),
+        //     const SizedBox(height: 20,),
+        //     ElevatedButton(
+        //       onPressed: (){
+        //         Navigator.push(
+        //           context,
+        //           MaterialPageRoute(
+        //             builder: (context) => DeviceManagementApp(),
+        //       ),
+        //     );
+        //     },
+        //     child: const Text("Device Management")
+        //     ),
+        //     const SizedBox(height: 20,),
+        //     ElevatedButton(
+        //       onPressed: (){
+        //         Navigator.push(
+        //           context,
+        //           MaterialPageRoute(
+        //             builder: (context) => NotificationsPage(),
+        //       ),
+        //     );
+        //     },
+        //     child: const Text("Vista notificaciones")
+        //     ),
+        //     const SizedBox(height: 20,),
+        //     ElevatedButton(
+        //       onPressed: (){
+        //         Navigator.push(
+        //           context,
+        //           MaterialPageRoute(
+        //             builder: (context) => LoginPage(),
+        //       ),
+        //     );
+        //     },
+        //     child: const Text("Login")
+        //     ),
+        //     const SizedBox(height: 20,),
+        //     ElevatedButton(
+        //       onPressed: (){
+        //         Navigator.push(
+        //           context,
+        //           MaterialPageRoute(
+        //             builder: (context) => login_devices(),
+        //       ),
+        //     );
+        //     },
+        //     child: const Text("login devices")
+        //     )
+        //   ],
+        // ),
       ),
     );
   }
