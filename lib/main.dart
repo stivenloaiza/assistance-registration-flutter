1db1db19800e484ca3238ddb96ac9a150daf38f9
devBitan <jquiramadev@outlook.com>
2024-12-11 07:55:44 -0500

    fix: inicial orute login

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/', // Define la ruta inicial
+      initialRoute: '/login', // Define la ruta inicial
       routes: {
         '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
         '/register': (context) => RegisterPage(),
